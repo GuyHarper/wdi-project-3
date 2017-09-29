1c39eee3c8090ed68ce2ef112b447548faed0685
Gio85 <giovanni.galiero85@gmail.com>
2017-09-29 13:51:18 +0100

    developed all the routes

--- config/routes.js
+++ config/routes.js
@@ -5,4 +5,9 @@ router.route('/pets')
   .get(pets.index)
   .post(pets.create);
 
+router.route('/pets/:id')
+  .get(pets.show)
+  .put(pets.update)
+  .delete(pets.delete);
+
 module.exports = router;
--- controllers/pets.js
+++ controllers/pets.js
@@ -1,13 +1,14 @@
 const Pet = require('../models/pet');
 
-function petsCreate(req, res) {
+///************************** -------> remember to put next inside all the functions
+function createRoute(req, res) {
   Pet
     .create(req.body)
     .then(pets => res.status(201).json(pets))
     .catch(err => res.status(500).json(err));
 }
 
-function petsIndex(req, res) {
+function indexRoute(req, res) {
   Pet
     .find()
     .exec()
@@ -15,7 +16,52 @@ function petsIndex(req, res) {
     .catch(err => res.status(500).json(err));
 }
 
+function showRoute(req, res) {
+  Pet
+    .findById(req.params.id)
+    .exec()
+    .then((pet) => {
+      if(!pet) return res.notFound();
+
+      res.json(pet);
+    })
+    .catch(err => req.status(500).json(err));
+}
+
+function updateRoute(req, res) {
+
+  if(req.file) req.body.image = req.file.filename;
+
+  Pet
+    .findById(req.params.id)
+    .exec()
+    .then((pet) => {
+      if(!pet) return res.notFound();
+
+      pet = Object.assign(pet, req.body);
+      return pet.save();
+    })
+    .then((pet) => res.json(pet))
+    .catch(err => req.status(500).json(err));
+}
+
+function deleteRoute(req, res) {
+  Pet
+    .findById(req.params.id)
+    .exec()
+    .then((pet) => {
+      if(!pet) return res.notFound();
+
+      return pet.remove();
+    })
+    .then(() => res.status(204).end())
+    .catch(err => req.status(500).json(err));
+}
+
 module.exports = {
-  create: petsCreate,
-  index: petsIndex
+  create: createRoute,
+  index: indexRoute,
+  show: showRoute,
+  update: updateRoute,
+  delete: deleteRoute
 };
