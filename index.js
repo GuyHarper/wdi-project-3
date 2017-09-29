d79afa88f5c6b9777811d3fdf63a05a7d8b1a425
= <guy.harper@gmail.com>
2017-09-29 11:32:19 +0100

    Setting up globalToJSON

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const { port }    = require('./config/environment');
 
 app.use(express.static(`${__dirname}/public`));
 
+mongoose.plugin(require('./lib/globalToJSON'));
+
 app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
 app.listen(port, () => console.log(`Express has started on port: ${port}`));
