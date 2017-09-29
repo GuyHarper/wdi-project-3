ceb3d297680c980c4a71b403aa852bffeea9362a
= <guy.harper@gmail.com>
2017-09-29 16:50:30 +0100

    Removing secureRoute from routes

--- config/routes.js
+++ config/routes.js
@@ -7,13 +7,13 @@ const secureRoute = require('../lib/secureRoute');
 
 
 router.route('/pets')
-  .get(secureRoute, pets.index)
-  .post(secureRoute, pets.create);
+  .get(pets.index)
+  .post(pets.create);
 
 router.route('/pets/:id')
   .get(pets.show)
-  .put(secureRoute, pets.update)
-  .delete(secureRoute, pets.delete);
+  .put(pets.update)
+  .delete(pets.delete);
 
 router.route('/register')
   .post(auth.register);
@@ -24,12 +24,12 @@ router.route('/login')
 // ==========Messages router=============
 
 router.route('/user/:userId/messages')
-  .get(secureRoute, user.show)
-  .post(secureRoute, messages.create);
+  .get(user.show)
+  .post(messages.create);
 
 router.route('/user/:userId/messages/new')
-  .get(secureRoute, messages.new)
-  .delete(secureRoute, messages.delete);
+  .get(messages.new)
+  .delete(messages.delete);
 
 
 
