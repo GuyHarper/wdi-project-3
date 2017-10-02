ce4ada23fb215eb4c0b1b8ca9415fca252c4bb81
= <guy.harper@gmail.com>
2017-10-02 09:22:15 +0100

    Uncommenting messages

--- controllers/users.js
+++ controllers/users.js
@@ -14,24 +14,24 @@ function showRoute(req, res) {
     .catch(err => res.status(500).json(err));
 }
 
-// function usersShow(req, res) {
-//
-//   const data = {
-//     selectedUser: req.query.userId,
-//     user: User.findById(req.params.id).exec(),
-//     userMessages: Message.find({
-//       $or: [
-//         { to: req.currentUser.id, from: req.query.userId },
-//         { from: req.currentUser.id, to: req.query.userId }
-//       ]
-//     }).populate('to from').exec(),
-//     users: User.find().exec()
-//   };
-//
-//   Promise.props(data)
-//     .then(data => res.render('users/show', data))
-//     .catch(err => res.render('error', { err }));
-// }
+function usersShow(req, res) {
+
+  const data = {
+    selectedUser: req.query.userId,
+    user: User.findById(req.params.id).exec(),
+    userMessages: Message.find({
+      $or: [
+        { to: req.currentUser.id, from: req.query.userId },
+        { from: req.currentUser.id, to: req.query.userId }
+      ]
+    }).populate('to from').exec(),
+    users: User.find().exec()
+  };
+
+  Promise.props(data)
+    .then(data => res.render('users/show', data))
+    .catch(err => res.render('error', { err }));
+}
 
 module.exports = {
   show: showRoute
