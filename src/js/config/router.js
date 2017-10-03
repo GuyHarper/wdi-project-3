0f023444a51f5a9cec5d1ef28f9d1faf14809c2c
PullingStrings <drammatik@gmail.com>
2017-10-03 15:45:05 +0100

    homepage with signup

--- src/js/config/router.js
+++ src/js/config/router.js
@@ -46,6 +46,16 @@ function Router($stateProvider, $urlRouterProvider, $locationProvider) {
       url: '/home',
       templateUrl: 'js/views/pages/home.html',
       controller: 'HomeCtrl as home'
+    })
+    .state('HomeFoundForm', {
+      url: '/home',
+      templateUrl: 'js/views/pages/HomeFoundForm.html',
+      controller: 'HomeFoundCtrl as homeFound'
+    })
+    .state('HomeLostForm', {
+      url: '/home',
+      templateUrl: 'js/views/pages/HomeLostForm.html',
+      controller: 'HomeLostCtrl as homeLost'
     });
 
   $urlRouterProvider.otherwise('/pets');
--- src/js/controllers/pages.js
+++ src/js/controllers/pages.js
@@ -1,8 +1,44 @@
 angular
   .module('petsApp')
-  .controller('HomeCtrl', HomeCtrl);
+  .controller('HomeCtrl', HomeCtrl)
+  .controller('HomeFoundCtrl', HomeFoundCtrl)
+  .controller('HomeLostCtrl', HomeLostCtrl);
 
 function HomeCtrl(){
+  const vm = this;
+
+  function showLostForm() {
+    vm.lostFormSelected = true;
+    vm.foundFormSelected = false;
+  }
+
+  vm.showLostForm = showLostForm;
+
+  function showFoundForm() {
+    vm.foundFormSelected = true;
+    vm.lostFormSelected = false;
+  }
+
+  vm.showFoundForm = showFoundForm;
+}
+function HomeFoundCtrl(){
+  const vm = this;
+
+  function showLostForm() {
+    vm.lostFormSelected = true;
+    vm.foundFormSelected = false;
+  }
+
+  vm.showLostForm = showLostForm;
+
+  function showFoundForm() {
+    vm.foundFormSelected = true;
+    vm.lostFormSelected = false;
+  }
+
+  vm.showFoundForm = showFoundForm;
+}
+function HomeLostCtrl(){
   const vm = this;
 
   function showLostForm() {
