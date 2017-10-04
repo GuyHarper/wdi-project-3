ae22ee66c3f017b6728d339d5c3ae8e50d99b55b
PullingStrings <drammatik@gmail.com>
2017-10-04 09:29:25 +0100

    landing page

--- src/js/controllers/pages.js
+++ src/js/controllers/pages.js
@@ -1,18 +1,29 @@
 angular
   .module('petsApp')
   .controller('HomeCtrl', HomeCtrl);
-// .controller('LoginCtrl', LoginCtrl);
+// .controller('LoginCtrl', LoginCtrl)
+// .controller('RegisterCtrl', RegisterCtrl);
 
-
-function HomeCtrl(){
+HomeCtrl.$inject = ['$auth', '$state'];
+function HomeCtrl($auth, $state){
   const vm = this;
 
   function showLostForm() {
+
     vm.lostFormSelected = true;
     vm.foundFormSelected = false;
   }
 
   vm.showLostForm = showLostForm;
+
+  vm.credentials = {};
+
+  function submit() {
+    $auth.login(vm.credentials)
+      .then(() => $state.go('petsIndex'));
+  }
+
+  vm.submit = submit;
 
   function showFoundForm() {
     vm.foundFormSelected = true;
@@ -21,17 +32,33 @@ function HomeCtrl(){
 
   vm.showFoundForm = showFoundForm;
 
-  LoginCtrl.$inject = ['$auth', '$state'];
-  function LoginCtrl($auth, $state) {
-    const vm = this;
-    vm.credentials = {};
-
-    function submit() {
-      $auth.login(vm.credentials)
-        .then(() => $state.go('petsIndex'));
-    }
-
-    vm.submit = submit;
-  }
-
 }
+
+// LoginCtrl.$inject = ['$auth', '$state'];
+// function LoginCtrl($auth, $state) {
+//   const vm = this;
+//   vm.credentials = {};
+//
+//   function submit() {
+//     $auth.login(vm.credentials)
+//       .then(() => $state.go('petsIndex'));
+//   }
+//
+//   vm.submit = submit;
+// }
+//
+// RegisterCtrl.$inject = ['$auth', '$state'];
+// function RegisterCtrl($auth, $state) {
+//   const vm = this;
+//   vm.user = {};
+//
+//   function submit() {
+//     $auth.signup(vm.user)
+//       .then(() => {
+//         $state.go('login');
+//       });
+//   }
+//   vm.submit = submit;
+//
+//
+// }
