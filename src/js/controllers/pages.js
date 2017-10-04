0ab6e504429b14b3aefec70336fea5e943d6e66b
PullingStrings <drammatik@gmail.com>
2017-10-04 10:36:07 +0100

    register working

--- src/js/controllers/pages.js
+++ src/js/controllers/pages.js
@@ -1,8 +1,8 @@
 angular
   .module('petsApp')
-  .controller('HomeCtrl', HomeCtrl);
-// .controller('LoginCtrl', LoginCtrl)
-// .controller('RegisterCtrl', RegisterCtrl);
+  .controller('HomeCtrl', HomeCtrl)
+  // .controller('LoginCtrl', LoginCtrl)
+  .controller('RegisterCtrl', RegisterCtrl);
 
 HomeCtrl.$inject = ['$auth', '$state'];
 function HomeCtrl($auth, $state){
@@ -30,35 +30,31 @@ function HomeCtrl($auth, $state){
     vm.lostFormSelected = false;
   }
 
+  function authenticate(provider) {
+    console.log('in authenticate function');
+    $auth.authenticate(provider)
+      .then(() => $state.go('petsIndex'));
+  }
+
+  vm.authenticate = authenticate;
+
   vm.showFoundForm = showFoundForm;
 
 }
 
-// LoginCtrl.$inject = ['$auth', '$state'];
-// function LoginCtrl($auth, $state) {
-//   const vm = this;
-//   vm.credentials = {};
-//
-//   function submit() {
-//     $auth.login(vm.credentials)
-//       .then(() => $state.go('petsIndex'));
-//   }
-//
-//   vm.submit = submit;
-// }
-//
-// RegisterCtrl.$inject = ['$auth', '$state'];
-// function RegisterCtrl($auth, $state) {
-//   const vm = this;
-//   vm.user = {};
-//
-//   function submit() {
-//     $auth.signup(vm.user)
-//       .then(() => {
-//         $state.go('login');
-//       });
-//   }
-//   vm.submit = submit;
-//
-//
-// }
+
+RegisterCtrl.$inject = ['$auth', '$state'];
+function RegisterCtrl($auth, $state) {
+  const vm = this;
+  vm.user = {};
+
+  function submit() {
+    $auth.signup(vm.user)
+      .then(() => {
+        $state.go('login');
+      });
+  }
+  vm.submit = submit;
+
+
+}
